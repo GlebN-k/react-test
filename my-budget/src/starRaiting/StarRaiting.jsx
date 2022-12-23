dd146453d897334d60db533c9d1dabd43aa00e77
nizhnikgleb <nizhnik.gleb@gmail.com>
2022-12-23 12:28:01 +0200

    created component StarRaiting

---
+++ my-budget/src/starRaiting/StarRaiting.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaStar } from "react-icons/fa"
+
+export default function StarRaiting() {
+    return (
+        <div>
+            <FaStar color="red" />
+            <FaStar color="red" />
+            <FaStar color="red" />
+            <FaStar color="grey" />
+            <FaStar color="grey" />
+        </div>
+    )
+}
